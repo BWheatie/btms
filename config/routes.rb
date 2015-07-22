585051741fe60f51df49171e09bae13b08af3670
Ben Wheat <BWheatie@gmail.com>
2015-07-21 23:00:37 -0500

    added articles model and changed homepage layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get :contact, to: "pages#contact"
+  get :about, to: "pages#about"
+  get :sports, to: "pages#sports"
+  get :authors, to: "pages#authors"
+  resources :posts
   root 'pages#index'
 end
---
+++ spec/factories/articles.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :article do
+    title "MyString"
+author "MyString"
+text "MyText"
+  end
+
+end
